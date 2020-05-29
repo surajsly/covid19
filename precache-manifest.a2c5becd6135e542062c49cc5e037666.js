64b78e78b19ccbaec94a3a7eca61298d8f2f96d8
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-05-29 15:59:37 +0530

    Updates

---
+++ precache-manifest.a2c5becd6135e542062c49cc5e037666.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197a1a9c046abca3b573f61aa9e9cc97",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "b929e44b250f7c465a14",
+    "url": "/covid19/static/css/main.64d48f99.chunk.css"
+  },
+  {
+    "revision": "544d049b89365df6ff53",
+    "url": "/covid19/static/js/2.f909621a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.f909621a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b929e44b250f7c465a14",
+    "url": "/covid19/static/js/main.88477728.chunk.js"
+  },
+  {
+    "revision": "8b3e25a7bfadade91521",
+    "url": "/covid19/static/js/runtime-main.f5c467eb.js"
+  },
+  {
+    "revision": "a049ed611a0196da4cb390e2c8ae2ecc",
+    "url": "/covid19/static/media/grayed.a049ed61.png"
+  },
+  {
+    "revision": "f3194e05b1d622eaddfad9222cea222d",
+    "url": "/covid19/static/media/newcrop.f3194e05.png"
+  },
+  {
+    "revision": "374c0a7ba3669d3b7dd2a201753fbf4f",
+    "url": "/covid19/static/media/sentimentAnalysis.374c0a7b.png"
+  },
+  {
+    "revision": "89ce99c69472b734a5fe0e177221af22",
+    "url": "/covid19/static/media/who4.89ce99c6.png"
+  }
+]);
